4fb1e24f810ef377782a305309e8c9301b214d90
yahia <yalaa90@gmail.com>
2018-09-04 07:16:34 +0200

    first

---
+++ 1/reddit-clone/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { browserHistory } from 'react-router';
+import App from './containers/App';
+import Routes from './routes';
+import registerServiceWorker from './containers/utils/registerServiceWorker';
+
+ReactDOM.render(
+    <Routes	history={browserHistory}	/>
+    , document.getElementById('root')
+);
+registerServiceWorker();
